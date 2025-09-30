326b3f009c36e378f270f6601a70a51401b044c8
Ye Yan <ye.yan@me.com>
2025-09-30 19:20:48 +1000

    minor changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,12 @@
 -- Basic Neovim options (add more here as needed)
-vim.opt.nu = true -- Line numbers
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.mouse = "a" -- Enable mouse
-vim.opt.termguicolors = true -- True color support
+vim.opt.nu = true                 -- Line numbers
+vim.opt.relativenumber = true     -- Relative line numbers
+vim.opt.mouse = "a"               -- Enable mouse
+vim.opt.termguicolors = true      -- True color support
 vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
-vim.opt.tabstop = 4 -- Tab width (visual)
-vim.opt.shiftwidth = 4 -- Indent width
-vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 4               -- Tab width (visual)
+vim.opt.shiftwidth = 4            -- Indent width
+vim.opt.expandtab = true          -- Convert tabs to spaces
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.undofile = true
@@ -25,11 +25,21 @@ vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { noremap = true })
 vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { noremap = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "term://*",
-	callback = function()
-		vim.cmd("startinsert")
-	end,
+    pattern = "term://*",
+    callback = function()
+        vim.cmd("startinsert")
+    end,
 })
 
 -- GUI Configuration
-vim.opt.guifont = { "Source Code Pro", ":h18" }
+vim.opt.guifont = { "Fira Code", ":h14" }
+
+-- Javascript only configurations
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact", "html" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.expandtab = true
+    end,
+})
--- lua/plugins/fmt.lua
+++ lua/plugins/fmt.lua
@@ -1,24 +1,30 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" }, -- Mason is crucial for installing stylua and rustfmt
-	event = { "BufWritePre" }, -- Only load conform when a buffer is about to be written
-	config = function()
-		require("conform").setup({
-			-- Enable format-on-save globally.
-			-- If you later add filetypes you DON'T want formatted on save,
-			-- you'd change this to a `callback` function.
-			format_on_save = {
-				timeout_ms = 700, -- Give formatters up to 700ms to complete
-				lsp_fallback = true, -- If no conform formatter found, try LSP for formatting
-			},
-
-			-- Define which formatters to use for each filetype
-			formatters_by_ft = {
-				lua = { "stylua" }, -- Use stylua for Lua files
-				rust = { "rustfmt" }, -- Use rustfmt for Rust files
-				-- Add other filetypes/formatters here if needed later
-				-- javascript = { 'prettier' },
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" }, -- Mason is crucial for installing stylua and rustfmt
+    event = { "BufWritePre" },       -- Only load conform when a buffer is about to be written
+    config = function()
+        require("conform").setup({
+            -- Enable format-on-save globally.
+            -- If you later add filetypes you DON'T want formatted on save,
+            -- you'd change this to a `callback` function.
+            format_on_save = {
+                timeout_ms = 700,    -- Give formatters up to 700ms to complete
+                lsp_fallback = true, -- If no conform formatter found, try LSP for formatting
+            },
+            formatters = {
+                prettier = {
+                    command = "/home/haskell/.nvm/versions/node/v24.5.0/bin/prettier",
+                },
+            },
+            -- Define which formatters to use for each filetype
+            formatters_by_ft = {
+                lua = { "stylua" },   -- Use stylua for Lua files
+                rust = { "rustfmt" }, -- Use rustfmt for Rust files
+                html = { "prettier" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                -- Add other filetypes/formatters here if needed later
+            },
+        })
+    end,
 }
